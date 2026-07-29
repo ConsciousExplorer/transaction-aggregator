06e9b1f8880a34172c156ff8d1cfc293fde1b31c
ThusByZarathustra <edwinstoltz.es@gmail.com>
2026-07-29 22:53:39 +0200

    add graceful shutdown

--- apps/consumer/src/app.ts
+++ apps/consumer/src/app.ts
@@ -4,6 +4,8 @@
  *
  */
 
+import { createPool } from "./integrations/database/postgres.ts";
+
 export async function startupCheck<T>(
 	name: string,
 	action: () => Promise<T>,
@@ -22,7 +24,25 @@ export async function startupCheck<T>(
 	}
 }
 
+export async shudownCheck<T>()
+
 await startupCheck(
 	"test",
 	() => new Promise((resolve) => setTimeout(resolve, 2000)),
 );
+
+const pool = await startupCheck("PostgreSQL", () =>
+	createPool({
+		min: 3,
+		max: 10,
+		database: "txn_agg",
+		host: "localhost",
+		port: 5432,
+		user: "admin",
+		password: "admin",
+	}),
+);
+
+console.log(await pool.query("Select 1=1"));
+
+process.on("SIGTERM", gracefulShutdown);
---
+++ apps/consumer/src/integrations/database/postgres.ts
@@ -0,0 +1,16 @@
+import { Pool, type PoolConfig } from "pg";
+
+export async function createPool(config: PoolConfig): Promise<Pool> {
+	const pool = new Pool({
+		min: 3,
+		max: 20,
+		idleTimeoutMillis: 30_000,
+		connectionTimeoutMillis: 10_000,
+		...config,
+	});
+
+	// Ensure that a connection is established and we can connect
+	await pool.query("SELECT 1");
+
+	return pool;
+}
