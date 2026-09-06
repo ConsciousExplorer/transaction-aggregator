99c6bcecbb153c626dc888659d9d4d4d8e0b7fad
ThusByZarathustra <edwinstoltz.es@gmail.com>
2026-09-06 16:10:51 +0200

    update kafkaUI to use oauth

--- infrastructure/postgres/initdb/01-admin.sql
+++ infrastructure/postgres/initdb/01-admin.sql
@@ -6,7 +6,17 @@ BEGIN
 END
 $$;
 
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'keycloak') THEN
+        CREATE ROLE keycloak LOGIN PASSWORD 'keycloak_password';
+    END IF;
+END
+$$;
+
 
 -- Create the database and assign ownership to the admin role
 -- This ensures that the superuser cannot delete the database, but the admin role can manage it.
 CREATE DATABASE txn_agg OWNER admin;
+CREATE DATABASE keycloak OWNER keycloak;
+
